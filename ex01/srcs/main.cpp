76a059e116969bab06f6d27113e9c512611e60a9
Xuna <romainchampliau@gmail.com>
2022-05-02 17:10:15 +0200

    Beginning of Ex01

---
+++ ex01/srcs/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rchampli <rchampli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/02 15:26:50 by rchampli          #+#    #+#             */
+/*   Updated: 2022/05/02 16:54:09 by rchampli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/phonebook.hpp"
+
+int phonebook()
+{
+	std::cout << "CHOICE: ";
+	getline(std::cin, /* NAME THE VARIABLE */);
+}
+
+int	main(int ac, char **av)
+{
+	phonebook();
+	return 0;
+}
